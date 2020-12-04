a0bff923918f15496919fa160ea29a6a61ecd2c3
Yigit <yertaylan67@gmail.com>
2020-12-04 16:22:37 +0300

    Update main.tf

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -3,4 +3,11 @@ resource "random_uuid" "uuid" {}
 resource "azurerm_resource_group" "rg" {
   name     = "rg-hello-tf-${random_uuid.uuid.result}"
   location = var.location
+  
+    tags = {
+    
+    Environment = "Production"
+    Workload = "SAP"
+
+  }
 }
